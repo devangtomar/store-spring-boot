b2f0ebe6bd228fadd724f7baf7df0b08a78bfcb4
devangtomar <devangtomar123@gmail.com>
2025-07-05 16:14:53 +0530

    Add server configuration and inject application name in HomeController

--- src/main/java/com/codewithdevang/store/HomeController.java
+++ src/main/java/com/codewithdevang/store/HomeController.java
@@ -1,14 +1,20 @@
 package com.codewithdevang.store;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
+    // Injecting the application name from application.properties
+    @Value("${spring.application.name}")
+    private String appName;
+
     @RequestMapping("/")
     public String index() {
         String viewName = getViewName();
         System.out.println(viewName);
+        System.out.println("Application Name: " + appName);
         return viewName;
     }
 
