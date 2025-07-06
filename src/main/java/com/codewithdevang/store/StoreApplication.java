d0743c00127f22867c1b3083a189a395b450ba95
devangtomar <devangtomar123@gmail.com>
2025-07-06 10:56:56 +0530

    Refactor OrderService to support dependency injection for payment services

--- src/main/java/com/codewithdevang/store/StoreApplication.java
+++ src/main/java/com/codewithdevang/store/StoreApplication.java
@@ -2,15 +2,16 @@ package com.codewithdevang.store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.annotation.Order;
 
 @SpringBootApplication
 public class StoreApplication {
 
     public static void main(String[] args) {
 //        SpringApplication.run(StoreApplication.class, args);
-        var orderPaypalService = new OrderService(new PayPalPaymentService());
-        var orderStripeService = new OrderService(new StripePaymentService());
-        orderStripeService.placeOrder();
+        var orderPaypalService = new OrderService();
+//        var orderPaypalService = new OrderService(new PayPalPaymentService());
+//        var orderStripeService = new OrderService(new StripePaymentService());
         orderPaypalService.placeOrder();
     }
 }
